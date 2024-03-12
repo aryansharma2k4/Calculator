844f2ec3ed2d6dd1d973daf9b8e352d961d88b26
aryansharma2k4 <aryansharma2k4@gmail.com>
2024-03-12 16:11:22 +0530

    Corrected the transition on all buttons and added the functioning of the precentage and sign button

--- script.js
+++ script.js
@@ -1,16 +1,136 @@
 let res = document.querySelector('.result');
 let alClr = document.querySelector('#ac');
+
+function styleReset(){
+      plus.style.backgroundColor = ''
+      subtraction.style.backgroundColor='';
+      multiplication.style.backgroundColor='';
+      division.style.backgroundColor='';
+      plus.style.color = ''
+      subtraction.style.color='';
+      multiplication.style.color='';
+      division.style.color='';
+}
+
+function styleChange(ele){
+      ele.style.backgroundColor = '#ffffff';
+      ele.style.color = '#ffad32';
+      ele.style.transition = 'color 0.5s ease';
+      ele.style.transition = 'background-color 0.5s ease';
+}
+
+function keyStyleChange(ele){
+      ele.style.backgroundColor = '#8a8a8a';
+      ele.style.transition = 'background-color 1s ease';
+      setTimeout(() => {
+            ele.style.backgroundColor = '';
+      }, 200);
+
+}
 alClr.addEventListener('click',function(){
       res.innerHTML = '0';
+      alClr.style.backgroundColor = '#ffffff';
+      alClr.style.transition = 'background-color 1s ease';
+      setTimeout(() => {
+            alClr.style.backgroundColor = '';
+      }, 200);
 },false);
+let op =0;
+
+let plus = document.querySelector('#addition');
+plus.addEventListener('click',function(){
+      styleChange(plus);
+      frpt = parseInt(res.innerHTML);
+      res.innerHTML = '';
+      op = 1;
+})
+let subtraction = document.querySelector('#subtraction');
+subtraction.addEventListener('click',function(){
+      styleChange(subtraction);
+      frpt = parseInt(res.innerHTML);
+      res.innerHTML = '';
+      op = 2;
+})
+let multiplication = document.querySelector('#multiplication');
+multiplication.addEventListener('click',function(){
+      styleChange(multiplication);
+      frpt = parseInt(res.innerHTML);
+      res.innerHTML = '';
+      op =3;
+})
+let division = document.querySelector('#division');
+division.addEventListener('click',function(){
+      styleChange(division);
+      frpt = parseInt(res.innerHTML);
+      res.innerHTML = '';
+      op =4;
+})
+let sign = document.querySelector('#sign');
+sign.addEventListener('click',function(){
+      sign.style.backgroundColor = '#ffffff';
+      sign.style.transition = 'background-color 1s ease';
+      setTimeout(() => {
+            sign.style.backgroundColor = '';
+      }, 200);
+      res.innerHTML = -res.innerHTML;
+      frpt = parseInt(res.innerHTML);
+})
+let percentage = document.querySelector('#percentage');
+percentage.addEventListener('click',function(){
+      percentage.style.backgroundColor = '#ffffff';
+      percentage.style.transition = 'background-color 1s ease';
+      setTimeout(() => {
+            percentage.style.backgroundColor = '';
+      }, 200);
+      frpt = parseInt(res.innerHTML);
+      res.innerHTML = frpt/100;
+})
+document.addEventListener('keydown', function(event) {
+      if (event.key === '+') {
+             styleChange(plus);
+             frpt = parseInt(res.innerHTML);
+             res.innerHTML = '';
+      op = 1;
+      }
+});
+document.addEventListener('keydown', function(event) {
+      if (event.key === '-') {
+            styleChange(subtraction);
+             frpt = parseInt(res.innerHTML);
+             res.innerHTML = '';
+      op = 2;
+      }
+});
+document.addEventListener('keydown', function(event) {
+      if (event.key === '*') {
+            styleChange(multiplication);
+             frpt = parseInt(res.innerHTML);
+             res.innerHTML = '';
+      op = 3;
+      }
+});
+document.addEventListener('keydown', function(event) {
+      if (event.key === '/') {
+            styleChange(division);
+             frpt = parseInt(res.innerHTML);
+             res.innerHTML = '';
+      op = 4;
+      }
+});  
+
+
 let one = document.querySelector('#one');
 one.addEventListener('click',function(){
+      plus.style.backgroundColor = '';
       if(res.innerHTML == '0'){
             res.innerHTML = one.innerHTML;
       }
       else{
             res.innerHTML = res.innerHTML + one.innerHTML;
       }
+      styleReset();
+      keyStyleChange(one);
+
 },false);
 let two = document.querySelector('#two');
 two.addEventListener('click',function(){
@@ -20,6 +140,10 @@ two.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + two.innerHTML;
       }
+       styleReset();
+      keyStyleChange(two);
+
+
 },false);
 let three = document.querySelector('#three');
 three.addEventListener('click',function(){
@@ -29,6 +153,9 @@ three.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + three.innerHTML;
       }
+       styleReset();
+      keyStyleChange(three);
+
 },false);
 let four = document.querySelector('#four');
 four.addEventListener('click',function(){
@@ -38,6 +165,9 @@ four.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + four.innerHTML;
       }
+       styleReset();
+      keyStyleChange(four);
+
 },false);
 let five = document.querySelector('#five');
 five.addEventListener('click',function(){
@@ -47,6 +177,9 @@ five.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + five.innerHTML;
       }
+       styleReset();
+      keyStyleChange(five);
+
 },false);
 let six = document.querySelector('#six');
 six.addEventListener('click',function(){
@@ -56,6 +189,9 @@ six.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + six.innerHTML;
       }
+       styleReset();
+      keyStyleChange(six);
+
 },false);
 let seven = document.querySelector('#seven');
 seven.addEventListener('click',function(){
@@ -65,6 +201,9 @@ seven.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + seven.innerHTML;
       }
+       styleReset();
+      keyStyleChange(seven);
+
 },false);
 let eight = document.querySelector('#eight');
 eight.addEventListener('click',function(){
@@ -74,6 +213,9 @@ eight.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + eight.innerHTML;
       }
+       styleReset();
+      keyStyleChange(eight);
+
 },false);
 let nine = document.querySelector('#nine');
 nine.addEventListener('click',function(){
@@ -83,6 +225,8 @@ nine.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + nine.innerHTML;
       }
+       styleReset();
+      keyStyleChange(nine);
 },false);
 let zero = document.querySelector('p#zero');
 zero.addEventListener('click',function(){
@@ -92,6 +236,9 @@ zero.addEventListener('click',function(){
       else{
             res.innerHTML = res.innerHTML + zero.innerHTML;
       }
+       styleReset();
+      keyStyleChange(zero);
+
 },false);
 
 document.addEventListener('keydown', function(event) {
@@ -102,6 +249,8 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '1';
             }
+            styleReset();
+            keyStyleChange(one);
       }
 });
 
@@ -113,6 +262,9 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '2';
             }
+            styleReset();
+            keyStyleChange(two);
+
       }
 });
 document.addEventListener('keydown', function(event) {
@@ -123,6 +275,8 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '3';
             }
+            styleReset();
+            keyStyleChange(three);
       }
 });
 document.addEventListener('keydown', function(event) {
@@ -133,6 +287,8 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '4';
             }
+            styleReset();
+            keyStyleChange(four);
       }
 });
 document.addEventListener('keydown', function(event) {
@@ -143,6 +299,8 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '5';
             }
+            styleReset();
+            keyStyleChange(five);
       }
 });
 document.addEventListener('keydown', function(event) {
@@ -153,6 +311,8 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '6';
             }
+            styleReset();
+            keyStyleChange(six);
       }
 });
 document.addEventListener('keydown', function(event) {
@@ -163,6 +323,8 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '7';
             }
+            styleReset();
+            keyStyleChange(seven);
       }
 });
 document.addEventListener('keydown', function(event) {
@@ -173,6 +335,8 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '8';
             }
+            styleReset();
+            keyStyleChange(eight);
       }
 });
 document.addEventListener('keydown', function(event) {
@@ -183,6 +347,8 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '9';
             }
+            styleReset();
+            keyStyleChange(nine);
       }
 });
 document.addEventListener('keydown', function(event) {
@@ -193,64 +359,55 @@ document.addEventListener('keydown', function(event) {
             else{
                   res.innerHTML = res.innerHTML + '0';
             }
+            styleReset();
+            keyStyleChange(zero);
       }
 });
 document.addEventListener('keydown', function(event) {
       if (event.key === 'Backspace') {
+            alClr.style.backgroundColor = '#ffffff';
+            alClr.style.transition = 'background-color 1s ease';
+            setTimeout(() => {
+                  alClr.style.backgroundColor = '';
+            }, 200);
             res.innerHTML = '0';
+            styleReset();
+            keyStyleChange();
       }
 });
-let op =0;
-
-let plus = document.querySelector('#addition');
-plus.addEventListener('click',function(){
-      plus.style.backgroundColor = '#ddc094';
-      frpt = parseInt(res.innerHTML);
-      res.innerHTML = '0';
-      op = 1;
-})
-let subtraction = document.querySelector('#subtraction');
-subtraction.addEventListener('click',function(){
-      subtraction.style.backgroundColor = '#ddc094';
-      frpt = parseInt(res.innerHTML);
-      res.innerHTML = '0';
-      op = 2;
-})
-let multiplication = document.querySelector('#multiplication');
-multiplication.addEventListener('click',function(){
-      multiplication.style.backgroundColor = '#ddc094';
-      frpt = parseInt(res.innerHTML);
-      res.innerHTML = '0';
-      op =3;
-})
-let division = document.querySelector('#division');
-division.addEventListener('click',function(){
-      division.style.backgroundColor = '#ddc094';
-      frpt = parseInt(res.innerHTML);
-      res.innerHTML = '0';
-      op =4;
-})
 let scpt = 0;
-let equal = document.querySelector('#equal');
-equal.addEventListener('click', function() {
+const equal = document.querySelector('#equal');
+
+function calculateResult() {
     scpt = parseInt(res.innerHTML);
     if (op == 1) {
-      res.innerHTML = frpt + scpt;
-   } 
-   else if (op == 2) {
-      res.innerHTML = frpt - scpt;
-   }
-    else if (op == 3) {
-      res.innerHTML = frpt * scpt;
-   }
-    else if (op == 4) {
-      res.innerHTML = frpt / scpt;
-  }
+        res.innerHTML = frpt + scpt;
+        frpt = parseInt(res.innerHTML);
+    } else if (op == 2) {
+        res.innerHTML = frpt - scpt;
+        frpt = parseInt(res.innerHTML);
+    } else if (op == 3) {
+        res.innerHTML = frpt * scpt;
+        frpt = parseInt(res.innerHTML);
+    } else if (op == 4) {
+        res.innerHTML = frpt / scpt;
+        frpt = parseInt(res.innerHTML);
+    }
+}
+
+equal.addEventListener('click', calculateResult);
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      equal.style.backgroundColor = '#ffffff';
+      equal.style.color = '#ffad32';
+      equal.style.transition = 'color 0.5s ease';
+      equal.style.transition = 'background-color 0.5s ease';
+      setTimeout(() => {
+            equal.style.backgroundColor = '';
+            equal.style.color = '';
+      }, 200);
+      event.preventDefault();
+      calculateResult();
+    }
 });
-
-
-
-
-
-
-
